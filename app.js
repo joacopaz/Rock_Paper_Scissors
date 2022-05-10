ac457c08499efbff9a9d87e33edeb18e25c7caa8
joacopaz <joacopaz@gmail.com>
2022-05-10 02:09:22 -0300

    Added custom styles and much more responsiveness

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 */
 
 const getStarted = () => {
-    roundsToPlay = document.getElementById("roundNumber").value;
+    roundsToPlay = document.querySelector('[name="roundNumber"]').value;
     if (roundsToPlay < 1) return
-    document.getElementById("welcome").style.display = "none";
+    document.querySelector('.landing').style.display = "none";
     document.getElementsByTagName("main")[0].style.display = "contents";
 
 };
@@ -56,7 +56,7 @@ const determineRoundWinner = (user, ai) => {
             return `You chose ${user.toUpperCase()} and the AI chose ${ai.toUpperCase()}. <br>Bummer, you have lost this round.`
         }
     } else {
-        return `This round is invalid, a choice was incorrect. You chose ${user.toUpperCase()} and the ai chose ${ai.toUpperCase()}. Please replay the round.`
+        return `This round is invalid, a choice was incorrect. You chose ${user?user.toUpperCase():'nothing'} and the ai chose ${ai.toUpperCase()}. Please replay the round.`
     }
 }
 
@@ -92,6 +92,7 @@ const submit = () => {
         document.getElementById('played').textContent = (`
         Please make a choice`);
         document.getElementById('played').style.color = 'red';
+        document.querySelector(".submit-choice").focus();
         // Check if there are still rounds left to be played
     } else if (roundNumber < roundsToPlay) {
         document.getElementById('played').style.color = '';
@@ -105,9 +106,12 @@ const submit = () => {
         document.getElementById('played').innerHTML = (`${determineRoundWinner(userChoice,aiChoice)}`)
         updateState();
         // reset all buttons:
-        document.querySelector('input[type=radio]:checked').checked = false;
-        resetStyles()
+        if (document.querySelector('input[type=radio]:checked')) {
+            document.querySelector('input[type=radio]:checked').checked = false;
+        }
+        choiceMade = false;
         userChoice = '';
+        resetStyles()
     } else {
         if (gameFinished && userScore > aiScore) {
             document.getElementById('played').style.color = 'green';
@@ -121,27 +125,26 @@ const submit = () => {
         }
     }
     if (roundsToPlay > roundNumber) {
-        document.getElementById(
-            "enquiry"
-        ).innerText = `What do you choose for round ${roundNumber + 1}?`;
+        document.getElementsByTagName(
+            "legend"
+        )[0].innerText = `What do you choose for round ${roundNumber + 1}?`;
     } else {
         gameFinished = true;
     }
-    if (roundNumber === 1) {
-        document.getElementById('played').style.border = 'blue solid 2px;'
-    }
-
 }
 
 const waitForRounds = () => {
     if (typeof roundsToPlay !== "undefined") {
+        document.getElementsByTagName(
+            "legend"
+        )[0].innerText = `What do you choose for round ${roundNumber + 1}?`;
         if (roundsToPlay === 1) {
-            document.getElementById(
-                "roundHeader"
+            document.querySelector(
+                ".roundHeader"
             ).innerText = `- ${roundsToPlay} round -`;
         } else {
-            document.getElementById(
-                "roundHeader"
+            document.querySelector(
+                ".roundHeader"
             ).innerText = `- ${roundsToPlay} rounds -`;
         }
     } else {
@@ -153,7 +156,6 @@ const styleSelection = (id) => {
     document.getElementById(id).style.outline = '2px rgb(11, 99, 188) solid';
     document.getElementById(id).style.color = 'rgb(11, 99, 188)';
     resetStyles();
-
 }
 const resetStyles = () => {
     document.querySelectorAll('input[type=radio]:not(:checked)').forEach(e => {
@@ -161,8 +163,61 @@ const resetStyles = () => {
         e.parentElement.style.outline = '';
         e.parentElement.style.color = '';
     })
+    resetUser();
 }
 
+function setStyle(element, att, value) {
+    document.querySelector(element).style[att] = value;
+}
+
+const thinkingRock = () => {
+    if (choiceMade) return;
+    document.getElementById('rock-image-user').style.opacity = '100%';
+    document.querySelector('.user-input span').style.display = "none";
+}
+const thinkingPaper = () => {
+    if (choiceMade) return;
+    document.getElementById('paper-image-user').style.opacity = '100%';
+    document.querySelector('.user-input span').style.display = "none";
+}
+const thinkingScissors = () => {
+    if (choiceMade) return;
+    document.getElementById('scissors-image-user').style.opacity = '100%';
+    document.querySelector('.user-input span').style.display = "none";
+}
+const updateContainer = () => {
+    choiceMade = true;
+    document.getElementById('rock-image-user').style.opacity = '';
+    document.getElementById('paper-image-user').style.opacity = '';
+    document.getElementById('scissors-image-user').style.opacity = '';
+    document.querySelector('.user-input span').style.display = "none";
+
+    switch (userChoice) {
+
+        case 'rock':
+            document.getElementById('rock-image-user').style.opacity = '100%';
+            break;
+        case 'paper':
+            document.getElementById('paper-image-user').style.opacity = '100%';
+            break;
+        case 'scissors':
+            document.getElementById('scissors-image-user').style.opacity = '100%';
+            break;
+
+        default:
+            break;
+    }
+}
+
+const resetUser = () => {
+    if (choiceMade) return;
+    document.getElementById('rock-image-user').style.opacity = '';
+    document.getElementById('paper-image-user').style.opacity = '';
+    document.getElementById('scissors-image-user').style.opacity = '';
+    document.querySelector('.user-input span').style.display = "contents";
+}
+
+let choiceMade = false;
 let roundsToPlay;
 let userScore = 0;
 let aiScore = 0;
@@ -170,3 +225,5 @@ let userChoice;
 let aiChoice;
 let roundNumber = 0;
 let gameFinished = false;
+
+// Adding listeners for keys so user can press spacebar to action the submit button
